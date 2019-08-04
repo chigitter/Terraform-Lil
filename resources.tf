f967e2e07a26a8c8f1941c65e134c46d78e922a6
mygitvenkat <venkata.thota@grainger.com>
2019-08-04 17:34:16 -0500

    Added subnets

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "our_development_network" {
   name = "devnetwork"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
 }
 
 resource "aws_vpc" "environment-example-two" {
@@ -10,4 +10,31 @@ resource "aws_vpc" "environment-example-two" {
   tags = {
     Name = "terraform-aws-vpc-example-two"
   }
+}
+
+resource "aws_subnet" "subnet1" {
+cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3,1)}"
+vpc_id = "${aws_vpc.environment-example-two.id}"
+availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2,2)}"
+vpc_id = "${aws_vpc.environment-example-two.id}"
+availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+
 }
